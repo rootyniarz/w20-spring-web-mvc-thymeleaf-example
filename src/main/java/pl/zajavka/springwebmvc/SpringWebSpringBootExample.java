bede1270aed54d0996e3ce3f4d7664e6f70113f9
Damian <rootyniarz@gmail.com>
2024-01-14 01:03:12 +0100

    1. some changes pause at Warsztat #20 - SPRING BOOT,, #4 @SpringBootApplication - konfiguracja 16:45

---
+++ src/main/java/pl/zajavka/springwebmvc/SpringWebSpringBootExample.java
@@ -0,0 +1,11 @@
+package pl.zajavka.springwebmvc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSpringBootExample {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebSpringBootExample.class, args);
+
+    }
+}
